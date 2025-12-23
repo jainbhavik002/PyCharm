70c74b4717fca3a111670da5e041cdc49808635a
bhavik <jainbhavik002@gmail.com>
2025-12-23 14:32:37 +0530

    rename file

--- pwk.py
+++ pwk.py
@@ -1,12 +1,12 @@
-import message as pwk
+import pywhatkit as pwk
 
 # Define recipient details and message
-phone_number = "Your_Phone_Number"  # Ensure this includes a valid country code
-message = "Hello World"
+phone_number = "+919312868885"  # Ensure this includes a valid country code
+message = "Hello Mummy"
 
 try:
     # Schedule message at least 2 minutes ahead of current time
-    pwk.sendwhatmsg(phone_number, message, 24, 25)# Add time in 24hrs format: H, M
+    pwk.sendwhatmsg(phone_number, message, 13, 36)# Add time in 24hrs format: H, M
     print("Message scheduled successfully!")
 except Exception as e:
     print(f"An error occurred: {e}")
